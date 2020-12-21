71b0f59a8503982f40e5d03c0648fae817b17b1a
Rhiddhi Prasad Das <rhiddhiprasad@gmail.com>
2020-12-21 18:58:45 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+dir = "/home/rapidfire69/RaPiD/"
+tb = 0
+def tree(dir):
+    global tb
+    try:
+        tb += 2
+        if(os.path.isdir(dir)):
+            #print(" "*(tb-1)+os.path.basename(dir))
+            for i in os.listdir(dir):
+                print("| "*int(tb/2)+"-> "+i)
+                tree(os.path.join(dir,i))
+                tb -= 2
+        else:
+            pass
+    except:
+        print("!Permission Denied!!")
+        pass
+print(dir)
+tree(dir)
